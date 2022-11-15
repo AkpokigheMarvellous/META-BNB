d01a62edcf99f7c6df66825b6987348292cf8ae5
Akpokighe Marvellous O <m.ovigwe@yahoo.com>
2022-11-15 11:11:28 +0100

    Style - mobile & desktop view readjusted

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -5,7 +5,7 @@ const Brands = () => {
     <div className="flex items-center justify-between py-3 lg:px-20 md:px-10 px-5 bg-primary">
       <img src="/mbtoken.svg" alt="mbtoken" className="md:w-52 w-24" />
       <img src="/metamask.svg" alt="metamask" className="md:w-52 w-24" />
-      
+      <img src="/opensea.svg" alt="opensea" className="md:w-52 w-24" />
     </div>
   );
 };
